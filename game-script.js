accd746c9c05832098bf3d1d230a53119336979e
Sam Benning <bensam1993@gmail.com>
2021-06-26 17:26:04 -0400

    Added game() function. Game is now functional in console.

--- game-script.js
+++ game-script.js
@@ -1,5 +1,46 @@
+const choices = ["rock", "paper","scissors"];
+
 function computerPlay() {
-    const choices = ["rock", "paper","scissors"];
     const choice = Math.floor(Math.random()*3);
-    return choices[choice];
+    return choice;
+};
+
+function playRound(playerSelection, computerSelection) {
+
+
+    playerSelection++;
+    computerSelection++;
+
+    if (playerSelection===computerSelection) {
+        console.log("Tie game!");
+        return 0;
+    } else if ((playerSelection - computerSelection + 1) % 3 === 0) {
+        console.log("You Lose! " + choices[computerSelection-1] + " beats " + choices[playerSelection-1]);
+        return 1;
+    } else {
+        console.log("Victory! " + choices[playerSelection-1] + " beats " + choices[computerSelection-1]);
+        return 2;
+    }
+}
+
+function game() {
+
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (i=0; i<5;i++) {
+        let playerSelection = choices.indexOf(prompt().toLowerCase());
+        let computerSelection = computerPlay();
+        let result = playRound(playerSelection,computerSelection);
+        if (result === 1) {
+            computerScore++;
+        } else if (result ===2) {
+            playerScore++;
+        }
+    }
+
+    console.log("Game over!");
+    console.log("Your score is " + playerScore);
+    console.log("Computer score is " + computerScore);
+
 }
